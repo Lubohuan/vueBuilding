51d9ab83b9302d716e606bd09c85e10bf56c5aaf
陈耀宏 <chenyaohong@1357.cn>
2018-11-25 11:37:49 +0800

    更新 progressPicture.vue

--- src/components/progressPIcture/progressPicture.vue
+++ src/components/progressPIcture/progressPicture.vue
@@ -1,7 +1,7 @@
 <template>
 <!--任务预警-->
 <div class="progressPicture" style="height:100%;">
-     <iframe src="/static/guant/gantt.html" frameborder="0" width="100%" height="100%" ></iframe>
+     <iframe src="static/guant/gantt.html" frameborder="0" width="100%" height="100%" id="11"></iframe>
   </div>
 </template>
 
