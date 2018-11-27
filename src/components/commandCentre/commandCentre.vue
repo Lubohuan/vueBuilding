84a2c9690f81f226f08fd9422da0e250812f0827
陈耀宏 <chenyaohong@1357.cn>
2018-11-27 20:57:56 +0800

    update

--- src/components/commandCentre/commandCentre.vue
+++ src/components/commandCentre/commandCentre.vue
@@ -60,8 +60,8 @@
         <div class="commandCards">
             <div class="cradContents">
                 <span class="desp_looks">产值完成情况</span>
-                <el-progress :stroke-width="18" :percentage="$common.fomatPrecent(panTable.finishOutputRate)"></el-progress>
-                <!-- <el-progress :stroke-width="18" :percentage="panTable.finishOutputRate"></el-progress> -->
+                <el-progress v-if="!panTable.finishOutputRate" :stroke-width="18" :percentage="0"></el-progress>
+                <el-progress v-else :stroke-width="18" :percentage="$common.fomatPrecent(panTable.finishOutputRate)"></el-progress>
                 </div>
                 <div class="cardFooters">
                 <span>同周比：
@@ -85,8 +85,8 @@
         <div class="commandCards">
            <div class="cradContents">
                 <span class="desp_looks">合同工期进度</span>
-                <el-progress :stroke-width="18" :percentage="$common.fomatPrecent(panTable.contractProRate)"></el-progress>
-                <!-- <el-progress :stroke-width="18" :percentage="panTable.contractProRate"></el-progress> -->
+                <el-progress v-if="!panTable.contractProRate" :stroke-width="18" :percentage="0"></el-progress>
+                <el-progress v-else :stroke-width="18" :percentage="$common.fomatPrecent(panTable.contractProRate)"></el-progress>
                 </div>
             <div class="cardFooters">
               <span>同周比：
@@ -288,21 +288,20 @@ export default {
         switch(this.tabPosition){
         case "最近七天":
         this.queryType = 0;
-        this.endTime   = this.getDay(0);//当天日期
-        this.startTime = this.getDay(-6);//7天前日期
+        this.endTime   = this.$common.getDay(0);//当天日期
+        this.startTime = this.$common.getDay(-6);//7天前日期
         this.getAllDays(6);
         this.refreshList();
         break;
         case "本周":
         this.queryType = 1;
-        this.getMonDate();
         this.getWeekTime();
         this.refreshList();
         break;
         case "上周":
         this.queryType = 2;
-        this.startTime = this.getTimes(7);//上周开始时间
-        this.endTime = this.getTimes(1);//上周结束时间
+        this.startTime = this.$common.getTimes(7);//上周开始时间
+        this.endTime = this.$common.getTimes(1);//上周结束时间
         this.getLastAllDays(7);
         this.refreshList();
         break;
@@ -342,87 +341,10 @@ export default {
           console.log(error);
         });
     },
-    
-    //小于10天前面拼接0
-     doHandleMonth(month){  
-       var m = month;  
-       if(month.toString().length == 1){  
-          m = "0" + month;  
-       }  
-       return m;  
-    },
-
-     //输入数字查询日期
-    getDay(day){  
-       var today = new Date();  
-       var targetday=today.getTime() + 1000*60*60*24*day;          
-       today.setTime(targetday); 
-         
-       var tYear = today.getFullYear();  
-       var tMonth = today.getMonth();  
-       var tDate = today.getDate();  
-       tMonth = this.doHandleMonth(tMonth + 1);  
-       tDate  =  this.doHandleMonth(tDate);  
-       return tYear+"-"+tMonth+"-"+tDate;  
-    }, 
-     
-    //获取本周一日期
-    getMonDate(){
-        var d=new Date(),
-        day  = d.getDay(),
-        date = d.getDate();
-        if(day==1)
-        this.nowWeekDate = d;
-        if(day==0)
-        d.setDate(date-6);
-        else
-        d.setDate(date-day+1);
-        this.nowWeekDate = d;
-    },
-
-     //获取本月时间范围
-    getCurrentMonth(){
-        //起止日期数组  
-        var startStop = new Array();
-        //获取当前时间  
-        var currentDate =new Date;
-        //获得当前月份0-11  
-        var currentMonth = currentDate.getMonth();
-        //获得当前年份4位年  
-        var currentYear = currentDate.getFullYear();
-        //求出本月第一天  
-        var firstDay = new Date(currentYear, currentMonth, 1);
-        var firstDate = firstDay.getDate();
-        if(firstDate<10){
-            firstDate = '0' + firstDate;
-        }
-        var firstDays = firstDay.getFullYear() + '-' + (firstDay.getMonth() + 1) + '-' + firstDate;
- 
-        //当为12月的时候年份需要加1  
-        //月份需要更新为0 也就是下一年的第一个月  
-        if (currentMonth == 11) {
-            currentYear++;
-            currentMonth = 0; //就为  
-        } else {
-            //否则只是月份增加,以便求的下一月的第一天  
-            currentMonth++;
-        }
-        //一天的毫秒数  
-        var millisecond = 1000 * 60 * 60 * 24;
-        //下月的第一天  
-        var nextMonthDayOne = new Date(currentYear, currentMonth, 1);
-        //求出上月的最后一天  
-        var lastDay = new Date(nextMonthDayOne.getTime() - millisecond);
-        var lastDays = lastDay.getFullYear() + '-' + (lastDay.getMonth() + 1) + '-' + lastDay.getDate();
-        //添加至数组中返回  
-        startStop.push(firstDays);
-        startStop.push(lastDays);
-        //返回  
-        return startStop;
-    },
-    
+      
     /*获取本周从周一开始到当天的的日期*/
     getWeekTime() {
+        this.nowWeekDate = this.$common.getMonDate();
         this.dateArr = [];
         this.dateArrs = [];
         var arr=[];
@@ -431,7 +353,7 @@ export default {
         this.nowWeekDate.setDate(this.nowWeekDate.getDate()+1);
         }
         this.dateArrs = arr;
-        this.endTime = this.getDay(0);//获取当天日期
+        this.endTime = this.$common.getDay(0);//获取当天日期
         var arrIndex = this.indexVf(this.endTime);//查找当天在本周日期数组中的索引值
         this.dateArrs = arr.slice(0,arrIndex);//取出从周一到当天的日期范围
         this.startTime = this.dateArrs[0];
@@ -455,65 +377,22 @@ export default {
     },
 
     
-     //获取最近7天内所有的天数
+    //获取最近7天内所有的天数
     getAllDays(index){
       this.dateArr = [];
       for(let i = 0;i<=index;i++){
-        let dateTime =  this.getDay((-i));
+        let dateTime =  this.$common.getDay((-i));
         this.dateArr.unshift(dateTime.substr(8,2));
       }
-    },
-
-    //获取上周起始时间结束时间;
-    getTimes(n) {
-        var now = new Date();
-        var year = now.getFullYear();
-        //因为月份是从0开始的,所以获取这个月的月份数要加1才行
-        var month = now.getMonth() + 1;
-        var date = now.getDate();
-        var day = now.getDay();
-        console.log(date);
-        //判断是否为周日,如果不是的话,就让今天的day-1(例如星期二就是2-1)
-        if (day !== 0) {
-            n = n + (day - 1);
-        } else {
-            n = n + day;
-        }
-        if (day) {
-            //这个判断是为了解决跨年的问题
-            if (month > 1) {
-            month = month;
-            }
-            //这个判断是为了解决跨年的问题,月份是从0开始的
-            else {
-            year = year - 1;
-            month = 12;
-            }
-        }
-        now.setDate(now.getDate() - n);
-        year = now.getFullYear();
-        month = now.getMonth() + 1;
-        date = now.getDate();
-        var s = year + "-" + (month < 10 ? ('0' + month) : month) + "-" + (date < 10 ? ('0' + date) : date);
-        return s;
     },
 
      //获取上周所有的天数
     getLastAllDays(index){
       this.dateArr = [];
       for(let i = 1;i<=index;i++){
-        let dateTime =  this.getTimes(i);
+        let dateTime = this.$common.getTimes(i);
         this.dateArr.unshift(dateTime.substr(8,2));
       }
-    },
-
-    //获取当天日期
-    getThisDays(){
-    var data = new Date();
-    var dayData1 = data.getFullYear();
-    var dayData2 = data.getMonth() + 1;
-    var dayData3 = data.getDate();
-    this.endTime   = dayData1 + "-" + dayData2 + "-" + dayData3;
     },
 
     //提醒督办
@@ -527,11 +406,10 @@ export default {
      getDashBoard()
         .then(response => {
           this.panTable = response.body;
-          if(this.panTable.contractProRate.indexOf("%") > -1){
-             this.panTable.contractProRate = this.panTable.contractProRate.substring(0,this.panTable.contractProRate.length-1);
-             console.log(this.panTable.contractProRate,"this.panTable.contractProRate")
-           
-          }
+        //   if(this.panTable.contractProRate.indexOf("%") > -1){
+        //      this.panTable.contractProRate = this.panTable.contractProRate.substring(0,this.panTable.contractProRate.length-1);
+        //      console.log(this.panTable.contractProRate,"this.panTable.contractProRate")         
+        //   }
           
         })
         .catch(error => {
@@ -540,13 +418,13 @@ export default {
     }
     },
     created() {
-    //  console.log(this.getCurrentMonth(),'222222222222222'); 
-    this.endTime = this.getDay(0);//当天日期
-    this.startTime = this.getDay(-6);//7天前日期
+    this.endTime = this.$common.getDay(0);//当天日期
+    this.startTime = this.$common.getDay(-6);//7天前日期
     this.getAllDays(6);
     this.refreshList();//发送请求
     this.refreshLists();
     this.refreshPan();
+    console.log(this.$common.getMonDate(),"22222");
     console.log( this.hasPerm(3),11); 
    
    
--- src/utils/common.js
+++ src/utils/common.js
@@ -6,8 +6,120 @@ export default {
     return spmUrl;
   },
 
+  //获取本周一日期
+  getMonDate() {
+    var d = new Date(),
+      day = d.getDay(),
+      date = d.getDate();
+    if (day == 1)
+      return d;
+    if (day == 0)
+      d.setDate(date - 6);
+    else
+      d.setDate(date - day + 1);
+    return d;
+  },
+
+  //小于10天前面拼接0
+  doHandleMonth(month) {
+    var m = month;
+    if (month.toString().length == 1) {
+      m = "0" + month;
+    }
+    return m;
+  },
+
+  //输入数字查询日期
+  getDay(day) {
+    var today = new Date();
+    var targetday = today.getTime() + 1000 * 60 * 60 * 24 * day;
+    today.setTime(targetday);
+
+    var tYear = today.getFullYear();
+    var tMonth = today.getMonth();
+    var tDate = today.getDate();
+    tMonth = this.doHandleMonth(tMonth + 1);
+    tDate = this.doHandleMonth(tDate);
+    return tYear + "-" + tMonth + "-" + tDate;
+  },
+
+  //获取上周起始时间结束时间;
+  getTimes(n) {
+    var now = new Date();
+    var year = now.getFullYear();
+    //因为月份是从0开始的,所以获取这个月的月份数要加1才行
+    var month = now.getMonth() + 1;
+    var date = now.getDate();
+    var day = now.getDay();
+    console.log(date);
+    //判断是否为周日,如果不是的话,就让今天的day-1(例如星期二就是2-1)
+    if (day !== 0) {
+      n = n + (day - 1);
+    } else {
+      n = n + day;
+    }
+    if (day) {
+      //这个判断是为了解决跨年的问题
+      if (month > 1) {
+        month = month;
+      }
+      //这个判断是为了解决跨年的问题,月份是从0开始的
+      else {
+        year = year - 1;
+        month = 12;
+      }
+    }
+    now.setDate(now.getDate() - n);
+    year = now.getFullYear();
+    month = now.getMonth() + 1;
+    date = now.getDate();
+    var s = year + "-" + (month < 10 ? ('0' + month) : month) + "-" + (date < 10 ? ('0' + date) : date);
+    return s;
+  },
+
+  //获取本月时间范围
+  getCurrentMonth() {
+    //起止日期数组  
+    var startStop = new Array();
+    //获取当前时间  
+    var currentDate = new Date;
+    //获得当前月份0-11  
+    var currentMonth = currentDate.getMonth();
+    //获得当前年份4位年  
+    var currentYear = currentDate.getFullYear();
+    //求出本月第一天  
+    var firstDay = new Date(currentYear, currentMonth, 1);
+    var firstDate = firstDay.getDate();
+    if (firstDate < 10) {
+      firstDate = '0' + firstDate;
+    }
+    var firstDays = firstDay.getFullYear() + '-' + (firstDay.getMonth() + 1) + '-' + firstDate;
+
+    //当为12月的时候年份需要加1  
+    //月份需要更新为0 也就是下一年的第一个月  
+    if (currentMonth == 11) {
+      currentYear++;
+      currentMonth = 0; //就为  
+    } else {
+      //否则只是月份增加,以便求的下一月的第一天  
+      currentMonth++;
+    }
+    //一天的毫秒数  
+    var millisecond = 1000 * 60 * 60 * 24;
+    //下月的第一天  
+    var nextMonthDayOne = new Date(currentYear, currentMonth, 1);
+    //求出上月的最后一天  
+    var lastDay = new Date(nextMonthDayOne.getTime() - millisecond);
+    var lastDays = lastDay.getFullYear() + '-' + (lastDay.getMonth() + 1) + '-' + lastDay.getDate();
+    //添加至数组中返回  
+    startStop.push(firstDays);
+    startStop.push(lastDays);
+    //返回  
+    return startStop;
+  },
+
   //获取月份数组
-  getMonthAll(begin,end) {
+  getMonthAll(begin, end) {
     var d1 = begin;
     var d2 = end;
     var dateArry = new Array();
@@ -15,39 +127,39 @@ export default {
     var s2 = d2.split("-");
     var mCount = 0;
     if (parseInt(s1[0]) < parseInt(s2[0])) {
-        mCount = (parseInt(s2[0]) - parseInt(s1[0])) * 12 + parseInt(s2[1]) - parseInt(s1[1])+1;
+      mCount = (parseInt(s2[0]) - parseInt(s1[0])) * 12 + parseInt(s2[1]) - parseInt(s1[1]) + 1;
     } else {
-        mCount = parseInt(s2[1]) - parseInt(s1[1])+1;
+      mCount = parseInt(s2[1]) - parseInt(s1[1]) + 1;
     }
     if (mCount > 0) {
-        var startM = parseInt(s1[1]);
-        var startY = parseInt(s1[0]);
-        for (var i = 0; i < mCount; i++) {
-            if (startM < 12) {
-                dateArry[i] =  startM>9 ? startM : "0" + startM;
-                startM += 1;
-            } else {
-                dateArry[i] =  startM > 9 ? startM : "0" + startM;
-                startM = 1;
-                startY += 1;
-            }
+      var startM = parseInt(s1[1]);
+      var startY = parseInt(s1[0]);
+      for (var i = 0; i < mCount; i++) {
+        if (startM < 12) {
+          dateArry[i] = startM > 9 ? startM : "0" + startM;
+          startM += 1;
+        } else {
+          dateArry[i] = startM > 9 ? startM : "0" + startM;
+          startM = 1;
+          startY += 1;
         }
+      }
     }
-    return dateArry;   
+    return dateArry;
   },
 
   //根据传入数获取当前时间前几月或者后几月
   Month(month) {
     var time = new Date();
-    time.setDate(time.getDate());//获取Day天后的日期 
+    time.setDate(time.getDate()); //获取Day天后的日期 
     var y = time.getFullYear();
     var d = time.getDate();
     var m;
-    if (time.getMonth() + month + 1>12){
-      y = y+1;
-      m = time.getMonth() + month - 11;//获取当前月份的日期 d
-    }else{
-      m = time.getMonth() + month + 1;//获取当前月份的日期 d
+    if (time.getMonth() + month + 1 > 12) {
+      y = y + 1;
+      m = time.getMonth() + month - 11; //获取当前月份的日期 d
+    } else {
+      m = time.getMonth() + month + 1; //获取当前月份的日期 d
     }
     return y + "-" + m + "-" + d;
   },
@@ -67,12 +179,12 @@ export default {
           mouth = 12;
           year = year - 1;
           break; //一月一日 再剪一天 年份减一 月份为12 日期为31
-          case 2:
-          case 4:
-          case 6:
-          case 8:
-          case 9:
-          case 11:
+        case 2:
+        case 4:
+        case 6:
+        case 8:
+        case 9:
+        case 11:
           date = 31;
           mouth = mouth - 1;
           break; //这些月一日 再剪一天  月份减一 日期为31
@@ -85,9 +197,9 @@ export default {
             mouth = mouth - 1;
           }
           break; //三月一日 再剪一天  月份减一 日期为根据是否是闰年来判断 日期
-          case 5:
-          case 7:
-          case 10:
+        case 5:
+        case 7:
+        case 10:
           date = 30;
           mouth = mouth - 1;
           break; //这些月一日 再剪一天  月份减一 日期为30
@@ -138,35 +250,35 @@ export default {
     } else if (date == 30) {
       //当日期为30 时
       switch (mouth) {
-          case 1:
-          case 3:
-          case 5:
-          case 7:
-          case 8:
-          case 10:
-          case 12:
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
           date = date + 1;
           break; //这些月份的时候一个月有31天 到30的时候再加一
-          case 4:
-          case 6:
-          case 9:
-          case 11:
+        case 4:
+        case 6:
+        case 9:
+        case 11:
           date = 1;
           mouth = mouth + 1;
           break; //这些月份的时候一个月有30天 到30的时候 日期变为1 月份加一
       }
     } else if (date == 31) {
       switch (mouth) {
-          case 1:
-          case 3:
-          case 5:
-          case 7:
-          case 8:
-          case 10:
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
           date = 1;
           mouth = mouth + 1;
           break; //这些月份的时候一个月有31天 到31的时候  日期为1月份加一
-          case 12:
+        case 12:
           date = 1;
           mouth = 1;
           year = year + 1;
