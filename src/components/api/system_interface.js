a696016460a40e8fdae74542e815fc347b2e777e
fenglei <fenglei@1357.cn>
2018-12-18 19:46:59 +0800

    切换到SPM正式环境

--- src/components/api/system_interface.js
+++ src/components/api/system_interface.js
@@ -4,16 +4,16 @@ import uploadRequest from '@/utils/uploadRequest';
 // const baseUrl = 'http://172.16.7.157:8080/bimScheduleService';
 import axios from 'axios'
 //请求url
-const baseUrl = 'http://autobuild.jiguantong.com/bimScheduleService';
-//const baseUrl = 'http://spm.jiguantong.com/bimScheduleService';
+//const baseUrl = 'http://autobuild.jiguantong.com/bimScheduleService';
+const baseUrl = 'http://spm.jiguantong.com/bimScheduleService';
 
 //云盘接口
-const alphaUrl = 'http://autobuild.jiguantong.com';
-// const alphaUrl = 'http://spm.jiguantong.com';
+//const alphaUrl = 'http://autobuild.jiguantong.com';
+const alphaUrl = 'http://spm.jiguantong.com';
 
 //退出url
-const spmUrl = 'http://autobuild.1357.cn/Admin/Login/public_login';
-//const spmUrl = 'http://spm.1357.cn/Admin/Login/public_login';
+//const spmUrl = 'http://autobuild.1357.cn/Admin/Login/public_login';
+const spmUrl = 'http://spm.1357.cn/Admin/Login/public_login';
 
 export function loginUrl(){
   return spmUrl;
